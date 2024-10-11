8d307320c00679fbee8c0a533d79fc5a34a39b7a
Harikrishna K <harikrishna.k@dbizsolution.com>
2024-10-11 16:38:31 +0530

    Updated changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be3785c808810c24"; }
+/******/ 		__webpack_require__.h = function() { return "29715e5f074e987e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
